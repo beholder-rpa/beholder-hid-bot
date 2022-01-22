8ddd6e2766006e4c14f4591a4ca41ff63b90f496
Sean McLellan <Oceanswave@users.noreply.github.com>
2022-01-22 16:26:00 -0500

    Fix boot/init. Change action to not include full path

--- beholder-hid-bot-image-builder-rpi4/lib/root/dotnetbeholder_userinit.sh
+++ beholder-hid-bot-image-builder-rpi4/lib/root/dotnetbeholder_userinit.sh
@@ -2,6 +2,7 @@
 
 rm -rf ~/beholder-hid-bot
 wget https://github.com/beholder-rpa/beholder-hid-bot/releases/download/cd-prerelease/beholder-hid-bot-arm64.tar.gz
+mkdir ~/beholder-hid-bot
 tar -xvf ./beholder-hid-bot-arm64.tar.gz -C ~/beholder-hid-bot
 rm beholder-hid-bot-arm64.tar.gz
 
